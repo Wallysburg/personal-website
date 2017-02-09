21b9766068d11d13882ace931e3e6a52180e4500
Charlie Davis <cdavis@accusoft.com>
2017-02-08 23:07:04 -0500

    get rid of all the useless state stuff
    This is all useless now since I deleted the stateful hamburger menu

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { prefixLink } from 'gatsby-helpers';
 
 import Logo from 'components/Logo';
-import Menu from 'components/menu';
 import Wrapper from 'components/wrapper';
 
 import { IndexLink, Link } from 'react-router';
@@ -11,33 +10,7 @@ import styles from './styles.module.css';
 import 'react-github-button/assets/style.css';
 
 export default class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menuActive: false,
-    };
-
-    this.toggleMenu = this.toggleMenu.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-  }
-
-  componentDidMount() {
-    // @TODO: Remove this event listener when menu isn't open.
-    document.addEventListener('keydown', this.handleKeyPress);
-  }
-
-  toggleMenu(isVisible) {
-    this.setState({ menuActive: typeof isVisible === 'undefined' ? !this.state.menuActive : isVisible });
-  }
-
-  handleKeyPress(event) {
-    if (event.keyCode === 27) {
-      this.toggleMenu(false);
-    }
-  }
   render() {
-    const { menuActive } = this.state;
-
     return (
       <div className={styles.header}>
         <Wrapper>
@@ -61,7 +34,6 @@ export default class Header extends Component {
             </IndexLink>
           </div>
         </Wrapper>
-        <Menu onNavClick={() => this.toggleMenu(false)} active={menuActive} />
       </div>
     );
   }
